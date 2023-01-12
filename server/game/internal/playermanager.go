776f10c8c19d6d9e2d589b65f5a7095ccb7a8bdd
wuyutaott <wuyutaoktm@163.com>
2023-01-12 17:54:37 +0800

    playermgr中的map锁保护

--- server/game/internal/playermanager.go
+++ server/game/internal/playermanager.go
@@ -4,10 +4,12 @@ import (
 	"github.com/wuyutaott/leaf/gate"
 	"github.com/wuyutaott/leaf/log"
 	"server/base"
+	"sync"
 )
 
 type PlayerManager struct {
 	PlayerMap map[uint32]*Player
+	lock      *sync.RWMutex
 }
 
 func NewPlayerManager() *PlayerManager {
@@ -18,23 +20,33 @@ func NewPlayerManager() *PlayerManager {
 
 func (p *PlayerManager) Init() {
 	p.PlayerMap = make(map[uint32]*Player)
+	p.lock = new(sync.RWMutex)
 }
 
 func (p *PlayerManager) Get(id uint32) *Player {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
 	return p.PlayerMap[id]
 }
 
 func (p *PlayerManager) AddPlayer(player *Player) {
 	log.Debug("PlayerManager AddPlayer")
+	p.lock.Lock()
+	defer p.lock.Unlock()
 	p.PlayerMap[player.ID] = player
 }
 
 func (p *PlayerManager) DelPlayer(id uint32) {
 	log.Debug("PlayerManager DelPlayer")
+	p.lock.Lock()
+	defer p.lock.Unlock()
 	delete(p.PlayerMap, id)
 }
 
 func (p *PlayerManager) DelPlayerByAgent(a gate.Agent) *Player {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
 	userData := a.UserData()
 	if userData != nil {
 		user := userData.(*base.User)
@@ -51,6 +63,8 @@ func (p *PlayerManager) DelPlayerByAgent(a gate.Agent) *Player {
 }
 
 func (p *PlayerManager) Close() {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
 	for _, player := range p.PlayerMap {
 		player.SaveSync()
 	}
